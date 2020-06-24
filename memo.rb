ddf2bb27a15651e789ac61b67453a4b6589db814
harukus <43bw.hana@gmail.com>
2020-06-24 16:31:21 +0900

    MethodOverride,attr_accessor追加

--- memo.rb
+++ memo.rb
@@ -3,59 +3,51 @@ require "sinatra/reloader"
 require "json"
 require "commonmarker"
 
+class Memo
+  attr_accessor :memos
 
-class Rack::MethodOverride
-  ALLOWED_METHODS = %w[POST]
+  def memos
+    File.open("memo.json", "r") do |f|
+        @memos = JSON.load(f)
+      end
+    @memos
+  end
 
-  def method_override(env)
-    req = Rack::Request.new(env)
-    method = req.params[METHOD_OVERRIDE_PARAM_KEY] || env[HTTP_METHOD_OVERRIDE_HEADER]
-    method.to_s.upcase
+  def post_memos(memos)
+    File.open("memo.json", "w") do |file|
+      JSON.dump(memos, file)
+    end
+  end
+
+  def save_id(file)
+    if file["memos"].empty?
+      last_id = 0
+    else
+      last_id = file["memos"][-1]["id"]
+    end
+    next_id = last_id + 1
+    @id = next_id
+  end
+
+  def list_memo(x, id)
+    if id == x["id"].to_s
+      @id = id
+      @memo = x["memo"]
+      @memo_to_html = CommonMarker.render_html(@memo, :DEFAULT)
+    end
+  end
+
+  def edit_memo(x, id)
+    if id == x["id"].to_s
+      x["memo"] = params[:memo]
+    end
   end
 end
 
-enable :method_override
-
-def get_memos
-  memos = nil
-  File.open("memo.json", "r") do |f|
-    memos = JSON.load(f)
-  end
-  memos
-end
-
-def post_memos(memos)
-  File.open("memo.json", "w") do |file|
-    JSON.dump(memos, file)
-  end
-end
-
-def save_id(file)
-  if file["memos"].empty?
-    last_id = 0
-  else
-    last_id = file["memos"][-1]["id"]
-  end
-  next_id = last_id + 1
-  @id = next_id
-end
-
-def list_memo(x, id)
-  if id == x["id"].to_s
-    @id = id
-    @memo = x["memo"]
-    @memo_to_html = CommonMarker.render_html(@memo, :DEFAULT)
-  end
-end
-
-def edit_memo(x, id)
-  if id == x["id"].to_s
-    x["memo"] = params[:memo]
-  end
-end
+memo = Memo.new
 
 get "/" do
-  @memos = get_memos["memos"]
+  @memos = memo.memos["memos"]
   erb :list
 end
 
@@ -65,7 +57,7 @@ end
 
 post "/memo" do
   @memo = params[:memo]
-  @memos = get_memos
+  @memos = memo.memos
   save_id(@memos)
   @memos["memos"].push({ id: @id, memo: @memo })
   post_memos(@memos)
@@ -73,7 +65,7 @@ post "/memo" do
 end
 
 get "/memo/edit/:id" do |id|
-  @memos = get_memos["memos"]
+  @memos = memo.memos["memos"]
   @memos.each { |x|
     list_memo(x, id)
   }
@@ -81,7 +73,7 @@ get "/memo/edit/:id" do |id|
 end
 
 get "/memo/:id" do |id|
-  @memos = get_memos["memos"]
+  @memos = memo.memos["memos"]
   @memos.each { |x|
     list_memo(x, id)
   }
@@ -89,7 +81,7 @@ get "/memo/:id" do |id|
 end
 
 patch "/memo/:id" do |id|
-  @memos = get_memos
+  @memos = memo.memos
   @memos["memos"].each { |x|
     edit_memo(x, id)
   }
@@ -99,7 +91,7 @@ end
 
 delete "/memo/:id" do |id|
   @id = id
-  @memos = get_memos
+  @memos = memo.memos
   @memos["memos"].delete_if { |x| id == x["id"].to_s }
   post_memos(@memos)
   redirect to("/")

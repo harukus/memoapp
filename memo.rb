6c8370d6f488cf6d833f69217a6c07352328adbc
harukus <43bw.hana@gmail.com>
2020-06-29 23:09:19 +0900

    コネクションを張るのを１回にした

--- memo.rb
+++ memo.rb
@@ -4,31 +4,29 @@ require "pg"
 require "commonmarker"
 
 class Memo
+  def initialize
+    @conn = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
+  end
   def list_all
-    connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-    connection.exec("SELECT * FROM memo")
+    @conn.exec("SELECT * FROM memo")
   end
 
   def add(content)
-    connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-    res = connection.exec("insert into memo (content) values ('#{content}') RETURNING id")
+    res = @conn.exec("insert into memo (content) values ('#{content}') RETURNING id")
     res.first["id"]
   end
 
   def get_content_by_id(id)
-    connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-    res = connection.exec("SELECT content FROM memo WHERE id = #{id}")
+    res = @conn.exec("SELECT content FROM memo WHERE id = #{id}")
     res.first["content"]
   end
 
   def update_content_by_id(id, content)
-    connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-    connection.exec("UPDATE memo SET content = '#{content}' WHERE id = #{id}")
+    @conn.exec("UPDATE memo SET content = '#{content}' WHERE id = #{id}")
  end
 
   def delete_by_id(id)
-    connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-    connection.exec("DELETE FROM memo WHERE id = #{id}")
+    @conn.exec("DELETE FROM memo WHERE id = #{id}")
   end
 end
 

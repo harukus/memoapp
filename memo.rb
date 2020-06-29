1c902b764768224d3f554a8abd702840a38a4cb4
harukus <43bw.hana@gmail.com>
2020-06-29 23:01:04 +0900

    classを使ったリファクタリング

--- memo.rb
+++ memo.rb
@@ -7,53 +7,36 @@ class Memo
   attr_accessor :memos
   def memos
     connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-    id = connection.exec("SELECT id FROM memo")
-    content = connection.exec("SELECT content FROM memo")
+    connection.exec("SELECT * FROM memo")
   end
 
-  def save_memos
-    File.open("memo.json", "w") do |file|
-      JSON.dump(@memos, file)
-    end
-  end
-
-  def next_id
-    if memos["memos"].empty?
-      return 1
-    end
-    memos["memos"][-1]["id"] +1
+  def add(content)
+    connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
+    res = connection.exec("insert into memo (content) values ('#{content}') RETURNING id")
+    res.first["id"]
   end
 
   def get_content_by_id(id)
-    content = nil
-    memos["memos"].each do |memo|
-      if id == memo["id"].to_s
-        content = memo["content"]
-      end
-    end
-    content
+    connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
+    res = connection.exec("SELECT content FROM memo WHERE id = #{id}")
+    res.first["content"]
   end
 
   def update_content_by_id(id, content)
-    @memos = memos
-    @memos["memos"].each do |memo|
-      if id == memo["id"].to_s
-        memo["content"] = content
-      end
-    end
+    connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
+    connection.exec("UPDATE memo SET content = '#{content}' WHERE id = #{id}")
  end
 
   def delete_by_id(id)
-    @memos = memos
-    @memos["memos"].delete_if { |memo| id == memo["id"].to_s }
+    connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
+    connection.exec("DELETE FROM memo WHERE id = #{id}")
   end
 end
 
 memo = Memo.new
 
 get "/" do
-  connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-  @memos = connection.exec("SELECT * FROM memo")
+  @memos = memo.memos
 
   erb :list
 end
@@ -63,40 +46,30 @@ get "/memo/new" do
 end
 
 post "/memo" do
-  connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-  res = connection.exec("insert into memo (content) values ('#{params[:content]}') RETURNING id")
-  redirect to("/memo/#{res.first['id']}")
+  id = memo.add(params[:content])
+  redirect to("/memo/#{id}")
 end
 
 get "/memo/edit/:id" do |id|
-  connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
   @id = id
-  res = connection.exec("SELECT content FROM memo WHERE id = #{@id}")
-  @content = res.first["content"]
-
+  @content = memo.get_content_by_id(id)
   erb :edit_form
 end
 
 get "/memo/:id" do |id|
-  connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
   @id = id
-  res = connection.exec("SELECT content FROM memo WHERE id = #{@id}")
-  @html = CommonMarker.render_html(res.first["content"], :DEFAULT)
+  content = memo.get_content_by_id(id)
+  @html = CommonMarker.render_html(content, :DEFAULT)
 
   erb :detail
 end
 
 patch "/memo/:id" do |id|
-  content = params[:content]
-  connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-  @id = id
-  res = connection.exec("UPDATE memo SET content = '#{params[:content]}' WHERE id = #{@id}")
-  redirect to("/memo/#{@id}")
+  memo.update_content_by_id(id, params[:content])
+  redirect to("/memo/#{id}")
 end
 
 delete "/memo/:id" do |id|
-  connection = PG.connect(host: "localhost", user: "postgres", dbname: "memoapp")
-  @id = id
-  connection.exec("DELETE FROM memo WHERE id = #{@id}")
+  memo.delete_by_id(id)
   redirect to("/")
 end
